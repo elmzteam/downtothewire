b21dd420f1c6d7eeced499c15e8104e91a4f469b
bluepichu <github@bluepichu.com>
2016-01-13 22:24:17 -0500

    Fixes to editor

--- client/js/admin.js
+++ client/js/admin.js
@@ -29,10 +29,6 @@ $(function() {
 });
 
 var submit = function() {
-	if(!confirm("Are you sure?")){
-		return
-	}
-
 	var val = editor.getValue()
 	var tags = $("#tags").val().split(" ")
 	var title = $("#title").val()
