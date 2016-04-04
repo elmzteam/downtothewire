668b0b85fbb025695959f228d76d562121163a85
Zach Wade <zacharywade@gmail.com>
2016-04-04 17:37:30 -0400

    added postinstall script

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,13 @@
 pushd .
-if [ ! -e "startup.sh" ]; then
+if [ ! -e "package.json" ]; then
 	cd ..
 fi
 
-if [ ! -e "startup.sh" ]; then
+if [ ! -e "package.json" ]; then
+	cd $@
+fi
+
+if [ ! -e "package.json" ]; then
 	echo "Please run from the project directory"
 	popd
 	exit -1
@@ -12,3 +16,7 @@ fi
 mkdir build
 mkdir posts
 mkdir render
+
+node_modules/gulp/bin/gulp.js build
+
+popd
